519fc8b934850e5749816536dae6ec2cd6128d3b
Mark Silverberg <mark.silverberg@airtable.com>
2023-07-31 08:43:12 -0500

    Rename `get_full_table` to be `query` and improve docs

--- streamlit_airtable/airtable_connection.py
+++ streamlit_airtable/airtable_connection.py
@@ -8,20 +8,30 @@ import pandas as pd
 class AirtableConnection(ExperimentalBaseConnection[Base]):
     """Basic st.experimental_connection implementation for DuckDB"""
 
+    # Establish a connection to Airtable using personal access token and base ID
     def _connect(self, **kwargs) -> Base:
         personal_access_token = self._secrets["personal_access_token"]
         base_id = self._secrets["base_id"]
         return Base(personal_access_token, base_id)
 
+    # Get the base schema
+    # Uses pyAirtable's metadata.get_base_schema method
+    # which calls https://airtable.com/developers/web/api/get-base-schema
     def get_base_schema(self, ttl: int = 3600, **kwargs) -> dict:
         base_schema = metadata.get_base_schema(self._instance)
         return base_schema
 
-    def get_full_table(self, table_id: str, ttl: int = 3600, **kwargs) -> pd.DataFrame:
+    # Query records from a table using pyAirtable's Table.all method
+    # which calls https://airtable.com/developers/web/api/get-records
+    # and paginates automatically. Parameters such as max_records, sort, view,
+    # formula, and more can be passed in using kwargs and the pyAirtable reference
+    # at https://pyairtable.readthedocs.io/en/stable/api.html#parameters
+    def query(self, table_id: str, ttl: int = 3600, **kwargs) -> pd.DataFrame:
         @cache_data(ttl=ttl)
-        def _get_full_table(table_id: str, **kwargs) -> pd.DataFrame:
-            all_records = self._instance.all(table_id, **kwargs)
+        def _query(table_id: str, **kwargs) -> pd.DataFrame:
+            table = self._instance.get_table(table_id)
+            all_records = table.all(**kwargs)
             all_records_fields = [record["fields"] for record in all_records]
             return pd.DataFrame.from_records(all_records_fields)
 
-        return _get_full_table(table_id, **kwargs)
+        return _query(table_id, **kwargs)
