b1e9525251122f2d0ae4e4256d4471a2c01a607e
Mark Silverberg <mark.silverberg@airtable.com>
2023-08-06 15:37:21 -0500

    Add comments

--- examples/explore_helpers.py
+++ examples/explore_helpers.py
@@ -8,14 +8,18 @@ def create_graph_from_base_schema(base_id, json_data):
     table_names = {}
     relationships = {}
     field_names = {}
+
+    # Loop through each table
     for table in json_data["tables"]:
         table_id = table["id"]
         table_names[table_id] = table["name"]
 
+        # Loop through the table's fields
         for field in table["fields"]:
             field_id = field["id"]
             field_names[field_id] = field["name"]
 
+            # For linked record fields, add the relationship to the graph
             if field["type"] == "multipleRecordLinks":
                 linked_table_id = field["options"]["linkedTableId"]
                 is_reversed = field["options"]["isReversed"]
@@ -26,7 +30,7 @@ def create_graph_from_base_schema(base_id, json_data):
                 )
                 relationships.setdefault(relationship_key, []).append(field_id)
 
-    # Add nodes and edges to the graph
+    # Add nodes to the graph
     for table_id, table_name in table_names.items():
         graph.node(
             table_id,
@@ -35,6 +39,7 @@ def create_graph_from_base_schema(base_id, json_data):
             target="_blank",
         )
 
+    # Add edges to the graph
     for (source_table_id, target_table_id), field_ids in relationships.items():
         edge_label = ", ".join(field_names[field_id] for field_id in field_ids)
         graph.edge(
