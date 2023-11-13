4e6a123e2a6e5aac482e71800a0c012600c2ab19
Mark Silverberg <mark@marksilver.net>
2023-11-13 16:37:56 -0600

    Update examples dependency versions

--- examples/explore.py
+++ examples/explore.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 
 from langchain.chat_models import ChatOpenAI
-from langchain.agents import create_pandas_dataframe_agent
+from langchain_experimental.agents.agent_toolkits.pandas.base import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
 
 from streamlit_airtable import AirtableConnection
